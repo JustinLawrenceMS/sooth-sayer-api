09c10d4f31cd716812b23df2a86ec5fbdbf426b6
Ubuntu <ubuntu@ip-172-31-36-89.ec2.internal>
2023-12-14 18:30:31 +0000

    refactor psr prettify code

--- routes/api.php
+++ routes/api.php
@@ -13,12 +13,11 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::get('v1/sooths/fortune', [SoothController::class, 'showSooth']);
 
 Route::get('v1/sooths/all', [SoothController::class, 'showAllSooths']);
-
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
